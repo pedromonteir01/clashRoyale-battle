3675f0977efea0965da5f48f99b8fde1c149c833
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2024-05-06 15:32:08 -0300

    feat: configuration libs

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const { Pool } = require('pg');
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'ds564',
+    database: 'clashroyaledb'
+});
